a1bdfc4e2f7cab5c485ebf8371ade5099906ac01
ZenCODE <zenkey.zencode@gmail.com>
2018-05-19 09:45:02 +0200

    Added dest folder

--- python/zensync/zensync.py
+++ python/zensync/zensync.py
@@ -8,10 +8,6 @@ import os
 from os.path import isdir, join, abspath
 
 
-path = r"./"
-dest = "/home/richard/Temp/"
-
-
 class SyncHandler(object):
     """
     Handles the synchronization task between two given folders.
@@ -21,7 +17,7 @@ class SyncHandler(object):
         for file_name in os.listdir(source):
             full_name = join(source, file_name)
             if isdir(full_name):
-                self.sync_folder(full_name)
+                self.sync_folder(full_name, join(dest, file_name))
             else:
                 UI.show_message("Got file {0}".format(full_name))
 
@@ -41,6 +37,9 @@ class UI(object):
 
 
 if __name__ == "__main__":
+    path = r"./"
+    dest = "/home/richard/Temp/"
+
     UI.show_splash()
     sync = SyncHandler()
     sync.sync_folder(abspath(path), dest)
