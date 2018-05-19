d7ded027528329eb3388c6e5c1860be53210cffc
ZenCODE <zenkey.zencode@gmail.com>
2018-05-19 09:28:53 +0200

    Added UI class

--- python/zensync/zensync.py
+++ python/zensync/zensync.py
@@ -15,10 +15,20 @@ def process_folder(folder):
     for file_name in os.listdir(folder):
         full_name = join(folder, file_name)
         if isdir(full_name):
-            clean_folder(full_name)
+            process_folder(full_name)
         else:
             print("Got file {0}".format(full_name))
 
 
-process_folder(path)
-print("Starting sync...")
+class UI(object):
+    """
+    Handles the presentation
+    """
+    @staticmethod
+    def show_splash():
+        print("\n".join(["=" * 11, "= ZenSync =", "=" * 11, "\n"]))
+
+
+if __name__ == "__main__":
+    UI.show_splash()
+    process_folder(path)
