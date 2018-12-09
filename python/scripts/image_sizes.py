f7954e9361acc4b664b2b112295a80c0356f9ab7
Zen-CODE <zenkey.zencode@gmail.com>
2018-12-09 08:27:06 +0200

    Implemented file generation

--- python/scripts/image_sizes.py
+++ python/scripts/image_sizes.py
@@ -1,35 +1,45 @@
 from os.path import join, isdir
 from os import listdir
-from mutagen.easyid3 import EasyID3
+import PIL.Image as PILImage
 
-log_file = "error.log"
 
-def check_tag(f_name):
-    """ Test f_name to seee if it has a valid mp3 tag. """    
-    try:
-        mp3info = EasyID3(f_name)
-        mp3info.items()
-        print("validated " + f_name)
-    except Exception as e:
-        print("Error processing: " + f_name)
-        with open(log_file, "a") as f:
-            f.write(f_name + '\r\n')
-
-def scan_folder(source):
+class ImageSize(object):
     """
-    Synchronizes the contents of the sources and destination folder.
+    Class managing image sizes.
     """
-    # Note: We use listdir i.s.o. walk to we need tighter control over
-    # iteration to maintain the mapping the the destination
-    files = listdir(source)
-    for item in files:
-        f_source = join(source, item)
-        if isdir(f_source):
-            scan_folder(f_source)
-        elif f_source.lower().find(".mp3") > -1:
-            check_tag(f_source)
-            
-            
+    exts = [".png", ".jpg"]
+
+    def gen_size_file(self, folder, out_file):
+        """Generate a file containing the image sizes.
+        """
+        sizes = []  # A list contained name and size tuples
+
+        files = listdir(folder)
+        for item in files:
+            for ext in ImageSize.exts:
+                if item.endswith(ext):
+                    sizes.append((item, self.get_size(join(folder, item))))
+
+        self.write_to_file(sizes, out_file)
+
+    @staticmethod
+    def write_to_file(name_size_list, out_file):
+        """ Write the name and size into the to specified file. """
+        with open(out_file, 'wb') as f:
+            for name, size in name_size_list:
+                f.write(str(name + " " + str(size) + "\n").encode('utf-8'))
+
+
+    @staticmethod
+    def get_size(img_path):
+        """ Return the tuple containing the width and height of the image"""
+        with PILImage.open(img_path) as img:
+            return img.size
+
+
 if __name__ == "__main__":
     # Initialize
-    scan_folder('/home/fruitbat/Music/')
+    ImageSize().gen_size_file(
+        '/home/fruitbat/Repos/kivy-speedtest/graphics/',
+        '/home/fruitbat/Repos/kivy-speedtest/graphics/sizes.txt')
+
