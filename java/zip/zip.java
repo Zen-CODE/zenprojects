ce8481859fb9eb3d66351bbe74df9b089a9432be
Zen-CODE <zenkey.zencode@gmail.com>
2018-12-14 20:23:58 +0200

    Added java zip class

---
+++ java/zip/zip.java
@@ -0,0 +1,17 @@
+/**
+ * This is test class to establish the mechanics behind working with zip files
+ * to assist with handling them in CAMI-Apps
+ * 
+ * To compile this file:
+ *   javac zip.java
+ * 
+ * This will compiled the class to a zip.class file. To run it:
+ *   java -cp . Zip
+ */
+class Zip
+{
+
+public static void main( String[] args ) {
+    System.out.println("Hello World!");
+}
+}
