43b1f778abc55009f489a596a00488ab837f6a70
Zen-CODE <zenkey.zencode@gmail.com>
2019-02-02 16:00:21 +0200

    Added model editing for the account info

--- python/django/capiccount/budget/models.py
+++ python/django/capiccount/budget/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    """
+    A model of the account information
+    """
+    bank = models.TextField(max_length=200)
+    type = models.TextField(max_length=200)
+    number = models.IntegerField()
+    models.DecimalField(decimal_places=2, max_digits=100)
+
+
+class Transaction(models.Model):
+    """
+    Model of a transaction as described in the imported CSV file.
+    """
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    sequence_no = models.IntegerField()
+    posting_date = models.DateField()
+    transaction_date = models.DateField()
+    description = models.TextField(max_length=200)
+    credit = models.DecimalField(decimal_places=2, max_digits=100)
+    balance = models.DecimalField(decimal_places=2, max_digits=100)
