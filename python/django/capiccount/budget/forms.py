3e41da383c8f4ca56726eb21fbf30049ba608188
Zen-CODE <zenkey.zencode@gmail.com>
2019-02-07 17:20:38 +0200

    Form post working

--- python/django/capiccount/budget/forms.py
+++ python/django/capiccount/budget/forms.py
@@ -5,5 +5,4 @@ class UploadCSVForm(forms.Form):
     """
     Handle the upload of the Capitec CSV file
     """
-    title = forms.CharField(max_length=50)
     file = forms.FileField()
--- python/django/capiccount/budget/views.py
+++ python/django/capiccount/budget/views.py
@@ -3,18 +3,22 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UploadCSVForm
 
 
-def index(request):
+def index(_request):
+    """ The landing page. """
     return HttpResponse("Hello, world. You're at the budget index.")
 
 
 def import_csv(request):
 
-    #return render(request, "import.html", {})
+    # return render(request, "import.html", {})
+    print("# Request.method: {0}".format(request.method))
     if request.method == 'POST':
         form = UploadCSVForm(request.POST, request.FILES)
         if form.is_valid():
-            #handle_uploaded_file(request.FILES['file'])
+            print("# Handle uploaded file!")
+            # handle_uploaded_file(request.FILES['file'])
             return HttpResponseRedirect('/success/url/')
     else:
         form = UploadCSVForm()
     return render(request, 'import.html', {'form': form})
+
