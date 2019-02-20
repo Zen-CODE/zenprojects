ec7ac27ad4d6e4aee2bb7a8134f8b77168e92597
ZenCODE <zenkey.zencode@gmail.com>
2019-02-20 16:25:48 +0200

    Create links on landing page

--- python/django/capiccount/budget/urls.py
+++ python/django/capiccount/budget/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('import_csv', views.import_csv, name='import'),
+    path('import_csv', views.import_csv, name='import_csv'),
     path('delete', views.delete, name='delete'),
     path('view_transactions', views.view_transactions,
          name='view_transactions'),
