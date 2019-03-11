2bf0a0d22861e4bcfe9ab632f78261b37532ee19
Zen-CODE <zenkey.zencode@gmail.com>
2019-03-11 22:03:17 +0200

    Implemented date filtering on retrieved object list

--- python/django/capiccount/budget/views.py
+++ python/django/capiccount/budget/views.py
@@ -78,8 +78,10 @@ def category_analysis(request):
         form = CategoryAnalysis(initial={'start_date': start_date,
                                          'end_date': end_date})
 
+    trans_list = Transaction.objects.filter(transaction_date__lte=end_date,
+                                            transaction_date__gte=start_date)
     return render(request, 'budget/category_analysis.html',
-                  {'form': form,
+                  {'form': form, 'trans_list': trans_list,
                    'start_date': start_date, 'end_date': end_date})
 
 
