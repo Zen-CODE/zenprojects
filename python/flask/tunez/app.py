a0f16ef7b5649e269cf2dea4f68a70398b8cca69
Zen-CODE <zenkey.zencode@gmail.com>
2019-04-09 23:08:18 +0200

    Added mock albums

--- python/flask/tunez/app.py
+++ python/flask/tunez/app.py
@@ -1,12 +1,29 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
+albums = [
+    {
+        'artist': 'Ace Of Base',
+        'album': 'Da Capo'
+    },
+    {
+        'artist': 'Astrix',
+        'album': 'Artcore'
+    }
+]
 
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+@app.route('/tunez/api/v1.0/albums', methods=['GET'])
+def get_albums():
+    return jsonify({'albums': albums})
 
 
 if __name__ == '__main__':
