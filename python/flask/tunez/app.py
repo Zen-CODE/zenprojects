7a391c68fbc9cf3de43a94779b24e747ed0a00f3
Zen-CODE <zenkey.zencode@gmail.com>
2019-04-10 21:19:20 +0200

    Added requirements, authentication

--- python/flask/tunez/app.py
+++ python/flask/tunez/app.py
@@ -1,7 +1,9 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from library import MusicLib
 from os.path import expanduser
+from flask_httpauth import HTTPBasicAuth
+
 
 albums = [
     {
@@ -20,6 +22,21 @@ app = Flask(__name__)
 lib = MusicLib(expanduser("~/Zen/Music/"))
 """ An instance of the MusicLib to serve our library information. """
 
+auth = HTTPBasicAuth()
+""" Object handling out authentication """
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'bob':
+        return 'chop'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 
 @app.route('/')
 def index():
@@ -27,16 +44,19 @@ def index():
 
 
 @app.route('/tunez/api/v1.0/artists', methods=['GET'])
+@auth.login_required
 def get_artists():
     return jsonify({'artists': lib.get_artists()})
 
 
 @app.route('/tunez/api/v1.0/albums/<artist>', methods=['GET'])
+@auth.login_required
 def get_albums(artist):
     return jsonify({'artists': lib.get_albums(artist)})
 
 
 @app.route('/tunez/api/v1.0/tracks/<artist>/<album>', methods=['GET'])
+@auth.login_required
 def get_tracks(artist, album):
     return jsonify({'artists': lib.get_tracks(artist, album)})
 
