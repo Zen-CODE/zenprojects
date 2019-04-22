fa11fe04f5917162888a07eb2ea8fdb1f9508620
Zen-CODE <zenkey.zencode@gmail.com>
2019-04-22 14:58:30 +0200

    :zap: Album cover now working!

--- react/zentunez/src/App.js
+++ react/zentunez/src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 
 
 class PlayerImage extends Component {
+  /**
+   * This component handles the display of the album cover, along with a
+   * button to update it
+   */
   constructor(props) {
     super(props);
-    this.state = {img_data: null, };
+    this.state = {img_src: "", };
   };
+
+  onClick(){
+    /** 
+     * Fetch the album cover and
+    */
+    const url = "http://127.0.0.1:5000/tunez/api/v1.0/" + "player/cover"
+    this.setState({img_src: url});
+  }
+
   render(){
     return (
       <div>
-        <button>Fetch Image</button>
-        <img alt="Album cover"></img>
+        <button onClick={ () => this.onClick() }>Fetch Image</button>
+        <img alt="Album cover" src={this.state.img_src}></img>
       </div>
     );
   }
