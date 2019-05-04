5c4bd6dbbebf0e324f96e7b199de3325790e53bd
Zen-CODE <zenkey.zencode@gmail.com>
2019-05-04 11:17:41 +0200

    :construction: Started with click support

--- react/zentunez/src/components/Library/Library.js
+++ react/zentunez/src/components/Library/Library.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Library.css';
+import { VDivider } from '../Divider/Divider.js'
 
 export class Library extends Component {
     /**
@@ -7,12 +8,31 @@ export class Library extends Component {
      */    
     constructor(props) {
         super(props);
-      };
+        this.state = {
+          artist: "-",
+          album: "-",
+          img_src: ""
+        }
+      };  
+
+    Click() {
+      console.log("I Have been clicked")
+
+    }
+
 
     render(){
       return (
         <div className="LibraryPanel">
           <p><b>ZenTunez Library</b></p>
+          <VDivider />
+          <p><b>Artist:</b>{ this.artist } </p>
+          <p><b>Album:</b>{ this.album } </p>
+          <VDivider />
+          <img className="album-cover" alt="Album cover" src={ this.props.img_src }></img>
+          <br />
+          <VDivider />
+          <button onClick={() => this.Click()}>Random album</button>
         </div>
       );
     }
