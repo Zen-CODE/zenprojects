814a4ee602f474ff16de32d4af6ca361e21e255b
Zen-CODE <zenkey.zencode@gmail.com>
2019-05-05 17:28:25 +0200

    :wrench: Add postData javascript function

--- react/zentunez/src/components/Library/Library.js
+++ react/zentunez/src/components/Library/Library.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Library.css';
 import { VDivider } from '../Divider/Divider.js'
+import { postData } from '../../lib/post.js'
 
 export class Library extends Component {
     /**
@@ -32,6 +33,15 @@ export class Library extends Component {
       )
     }
 
+    enqueueAlbum() {
+      /* Add the current album to the queue in the currently playing audio player    
+      */
+      console.log("About to POST data for adding to the queue");
+      postData(this.api_url + `player/folder_enqueue`, {arist: this.state.artist,
+                                                        album: this.state.album})
+        .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+        .catch(error => console.error(error));
+    }
 
     render(){
       return (
@@ -45,6 +55,7 @@ export class Library extends Component {
           <br />
           <VDivider />
           <button onClick={() => this.getRandomAlbum()}>Random album</button>
+          <button onClick={() => this.enqueueAlbum()}>Enqueue album</button>
         </div>
       );
     }
