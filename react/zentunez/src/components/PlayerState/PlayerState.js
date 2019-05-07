0e3f4d14298219dc766feaf9fdfccd18da6011ed
Zen-CODE <zenkey.zencode@gmail.com>
2019-05-07 21:51:53 +0200

    :zap: Implemented position as a aslider

--- react/zentunez/src/components/PlayerState/PlayerState.js
+++ react/zentunez/src/components/PlayerState/PlayerState.js
@@ -13,7 +13,8 @@ export class PlayerState extends Component {
           <p><b>Album :</b> { this.props.album }</p>
           <p><b>Track :</b> { this.props.track }</p>
           <p><b>State :</b> { this.props.state }</p>
-          <p><b>Position :</b> { parseInt(100 * this.props.position) }%</p>
+          <input type="range" min="0" max="100" value={ this.props.position * 100 } />
+          <br />
           <img className="album-cover" alt="Album cover" src={ this.props.img_src }></img>
         </div>
       );
