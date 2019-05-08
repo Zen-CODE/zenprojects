01229c1c74b7c36c24472c8af941567254c40806
Zen-CODE <zenkey.zencode@gmail.com>
2019-05-08 13:42:06 +0200

    :sparkles: Track listing working

--- react/zentunez/src/components/TrackList/TrackList.js
+++ react/zentunez/src/components/TrackList/TrackList.js
@@ -5,15 +5,11 @@ export class TrackList extends Component {
     /**
      * This component handles the display of the track list for an album
      */
-    constructor(props) {
-        super(props);
-        this.state = {tracks: ["Test track1", "Test track 2"]};
-      };
 
     render(){
       return (
         <div className="track-list" >
-            { this.state.tracks.map((item, index) => (
+            { this.props.tracks.map((item, index) => (
                 <li key={index}>{item}</li>
             ))}
         </div>
