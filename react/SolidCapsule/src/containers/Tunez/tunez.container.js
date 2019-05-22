667eb682db8dbd8f75305293eac8f30618042f6b
Zen-CODE <zenkey.zencode@gmail.com>
2019-05-22 22:36:28 +0200

    :wrench: added retrieval of user name

--- react/SolidCapsule/src/containers/Tunez/tunez.container.js
+++ react/SolidCapsule/src/containers/Tunez/tunez.container.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withWebId } from '@inrupt/solid-react-components';
-// import data from '@solid/query-ldflex';
+import data from '@solid/query-ldflex';
 import { withToastManager } from 'react-toast-notifications';
 
 /**
@@ -11,7 +11,8 @@ class TunezComponent extends Component<Props> {
     super(props);
 
     this.state = {
-      name: ''
+      name: '',
+      isLoading: false
     };
   }
   componentDidMount() {
@@ -19,6 +20,27 @@ class TunezComponent extends Component<Props> {
       this.getProfileData();
     }
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.webId && this.props.webId !== prevProps.webId) {
+      this.getProfileData();
+    }
+  }  
+
+  getProfileData = async () => {
+    this.setState({ isLoading: true });
+
+    /*
+     * This is an example of how to use LDFlex. Here, we're loading the webID link into a user variable. This user object
+     * will contain all of the data stored in the webID link, such as profile information. Then, we're grabbing the user.name property
+     * from the returned user object.
+     */
+    const user = data[this.props.webId];
+    const nameLd = await user.name;
+    const name = nameLd ? nameLd.value : '';
+
+    this.setState({ name: name, isLoading: false });
+  };
 
   render() {
     return (
