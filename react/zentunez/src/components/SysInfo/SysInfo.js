6a1b2112a47e9035ca5330185031e89993d3c742
Zen-CODE <zenkey.zencode@gmail.com>
2019-06-19 19:36:55 +0200

    :construction: Added mechaism for starting the timer

--- react/zentunez/src/components/SysInfo/SysInfo.js
+++ react/zentunez/src/components/SysInfo/SysInfo.js
@@ -10,12 +10,47 @@ export class SysInfo extends Component {
     this.state = { store: props.store,
                    show_sys_info: props.store.show_sys_info };
     this.unsubscribe = props.store.subscribe(() => this.storeChanged(props.store));
+    this.intervalID = null;
+    if (props.store.show_sys_info) { this.setTimer(true) }
   }
 
   storeChanged(store) {
     // React to changes in the shared stated
-    console.log("SysInfo. setting changed to " + store.getState().show_sys_info)
-    this.setState({ show_sys_info: store.getState().show_sys_info });
+    const si = store.getState().show_sys_info;
+    console.log("SysInfo. setting changed to " + si)
+    this.setState({ show_sys_info: si });
+    this.setTimer(si);
+  }
+
+  timerEvent(event) {
+    // The timer event has been fired
+    console.log("Timer fired.")
+  }
+
+  setTimer(on) {
+      // Switch the Timer on and off
+      if (on && (this.intervalID === null)){
+        console.log("Starting timer");
+        this.intervalID = setInterval(this.timerEvent);
+      } else if (!on && (this.intervalID != null)){
+        console.log("Killing timer");
+        clearInterval(this.intervalID);
+        this.intervalID = null
+      }
+  }
+
+  componentDidMount() {
+    // When our component loads, switch the timer on if the setting demands it
+    if (( this.intervalID === null) && (this.state.show_sys_info)) {
+        this.setTimer(true);
+    }
+  }
+
+  componentWillUnmount() {
+    // When out component unloads, destory the timer if it's running
+    if ( this.intervalID != null) {
+        this.setTimer(false);
+    }
   }
 
   render() {
