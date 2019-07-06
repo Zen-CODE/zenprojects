42ad00bf2d6bdbd0e741bf2a47ab4fdd50aec6b5
Zen-CODE <zenkey.zencode@gmail.com>
2019-07-06 13:58:18 +0200

    :bulb: Message queue now functional

--- react/zentunez/src/components/SysMsg/SysMsg.js
+++ react/zentunez/src/components/SysMsg/SysMsg.js
@@ -1,37 +1,49 @@
 
 import React, { Component } from 'react';
 
-
+/**
+* @typedef {Object} SysMsg
+* @property {Object} state - The object state as required by react
+* @property {number} unsubscribe - The identifier of the subscription to store event
+* @property {number} intervalID - The identifier of the timer interval event
+* @property {Array} messages - A list of messages in the notification queue
+*/
 export class SysMsg extends Component {
   /* This component contains the content of the settings popul. It
      also handles the saving on these settings
   */
   constructor(props){
     super(props);
-    this.state = { store: props.store,
-                   show_sys_info: props.store.show_sys_info };
+    this.state = { store: props.store, msg: '' };
     this.unsubscribe = props.store.subscribe(() => this.storeChanged(props.store));
     this.intervalID = null;
-    if (props.store.show_sys_info) { this.setTimer(true) }
+    this.messages = [];
   }
 
   storeChanged(store) {
-    // React to changes in the shared stated
-    const si = store.getState().show_sys_msg;
-    console.log("Got change message - " + si);
-    this.setState({ show_sys_info: si });
-    this.setTimer(si);
+    // React to the sending of messages. Place them in the queue and activate the
+    // timer.
+    const msg = store.getState().show_sys_msg;
+    this.messages.unshift(msg);
+    this.setTimer(true);
   }
 
   timerEvent(event) {
-    // The timer event has been fired
-    console.log("Timer fired.")
+    // The timer event has been fired. If there are messages to display. show
+    // them, otherwise remove the notification display
+    if (this.messages.length > 0) {
+      this.setState({ msg: this.messages.pop() })
+    } else {
+      this.setState({ msg: "" })
+      this.setTimer(false)
+    }
   }
 
   setTimer(on) {
-      // Switch the Timer on and off
+      // Switch the Timer on to start showing the message
       if (on && (this.intervalID === null)) {
-        this.intervalID = setInterval(this.timerEvent);
+        this.setState({ msg: this.messages.pop()});
+        this.intervalID = setInterval(this.timerEvent.bind(this), 2000);
       } else if (!on && (this.intervalID != null)) {
         clearInterval(this.intervalID);
         this.intervalID = null
@@ -54,9 +66,9 @@ export class SysMsg extends Component {
 
   render() {
     // We only return the component if enabled
-    if ( this.state.show_sys_info) {
+    if ( this.state.msg != "" ) {
         return (
-            <div>Sysinfo Dude! </div>)
+            <div>{ this.state.msg }</div>)
     } else {
         return null
     };
