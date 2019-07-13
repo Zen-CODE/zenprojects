d981622f46fb445f967e7311d945207b6bdc78ce
Zen-CODE <zenkey.zencode@gmail.com>
2019-07-13 10:11:59 +0200

    :wrench: Added support for saving

--- python/django/tunez/tunez/player/cloud_sql.py
+++ python/django/tunez/tunez/player/cloud_sql.py
@@ -3,16 +3,26 @@ This module houses code that interacts with the CloudSQL Postgress instance
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, create_engine
+from sqlalchemy.orm import sessionmaker
+from logging import getLogger
 
 
 Base = declarative_base()
+logger = getLogger(__name__)
 
 
 class NowPlaying(Base):
     """
     This class defines the model mapping the 'Now Playing' entries to the
     database table.
+
+    In order to create the tables:
+
+        engine = create_engine('sqlite:///:memory:', echo=True)
+        Base.metadata.create_all(engine)
+
     """
+    # SQL Alchemy
     __tablename__ = 'now_playing'
 
     id = Column(Integer, primary_key=True)
@@ -21,17 +31,38 @@ class NowPlaying(Base):
     state = Column(String)
     date = Column(Date)
 
+    # Our properties
+    Session = None
+
+    def __init__(self, **kwargs):
+        super(NowPlaying, self).__init__(**kwargs)
+        if NowPlaying.Session is None:
+            engine = create_engine(
+                env('SQLALCHEMY_DATABASE_URI'),
+                connect_args={'sslmode': 'prefer'})
+            self.Session = sessionmaker(bind=engine)
+
     def __repr__(self):
         return "<NowPlaying(artist='{0}', album='{1}', state='{2}'".format(
             self.artist, self.album, self.state)
 
+    def save(self):
+        """ Store the item in the database """
+        logger.info("Storing 'Now Playing' entry...")
+        session = self.Session()
+        session.add(self)
+        session.commit()
+        logger.info("Storing 'Now Playing' entry stored.")
+
 
 if __name__ == "__main__":
+    from datetime import datetime
     from environs import Env
     env = Env()
     env.read_env()
-    # engine = create_engine(env('SQLALCHEMY_DATABASE_URI'))
-    engine = create_engine('sqlite:///:memory:', echo=True)
-    Base.metadata.create_all(engine)
-    print("Database initialized")
+
+    obj = NowPlaying(artist="Us3", album="Cantaloop 2004", state="playing",
+                     date=datetime.now())
+    obj.save()
+
 
