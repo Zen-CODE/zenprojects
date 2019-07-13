de079ba68930b406dacf605b3c44f347bafd5fad
Zen-CODE <zenkey.zencode@gmail.com>
2019-07-13 10:35:10 +0200

    :zap: Started with actual writups to DB

--- python/django/tunez/tunez/player/cloud_sql.py
+++ python/django/tunez/tunez/player/cloud_sql.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, create_engine
 from sqlalchemy.orm import sessionmaker
 from logging import getLogger
-
+from os import environ
 
 Base = declarative_base()
 logger = getLogger(__name__)
@@ -34,13 +34,15 @@ class NowPlaying(Base):
     # Our properties
     Session = None
 
-    def __init__(self, **kwargs):
-        super(NowPlaying, self).__init__(**kwargs)
+    @staticmethod
+    def getSession():
+        """ Return an instantiated Session object. """
         if NowPlaying.Session is None:
             engine = create_engine(
-                env('SQLALCHEMY_DATABASE_URI'),
+                environ['SQLALCHEMY_DATABASE_URI'],
                 connect_args={'sslmode': 'prefer'})
-            self.Session = sessionmaker(bind=engine)
+            NowPlaying.Session = sessionmaker(bind=engine)
+        return NowPlaying.Session()
 
     def __repr__(self):
         return "<NowPlaying(artist='{0}', album='{1}', state='{2}'".format(
@@ -49,7 +51,7 @@ class NowPlaying(Base):
     def save(self):
         """ Store the item in the database """
         logger.info("Storing 'Now Playing' entry...")
-        session = self.Session()
+        session = self.getSession()
         session.add(self)
         session.commit()
         logger.info("Storing 'Now Playing' entry stored.")
@@ -61,8 +63,20 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
 
-    obj = NowPlaying(artist="Us3", album="Cantaloop 2004", state="playing",
-                     date=datetime.now())
-    obj.save()
+    # Save
+    # obj = NowPlaying(artist="Us3", album="Cantaloop 2004", state="playing",
+    #                  date=datetime.now())
+    # obj.save()
 
+    # Delete all
+    # session = NowPlaying.getSession()
+    # for instance in session.query(NowPlaying).order_by(NowPlaying.date):
+    #     print("Now playing: {0}".format(instance))
+    #     session.delete(instance)
+    # session.commit()
 
+    # List all
+    session = NowPlaying.getSession()
+    for instance in session.query(NowPlaying).order_by(NowPlaying.date)[:10]:
+        print("Now playing: {0}".format(instance))
+    print("Done")
