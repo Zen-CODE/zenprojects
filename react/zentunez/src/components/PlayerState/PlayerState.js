719c8adba32f51e95607f2d9fa2605052e7ad17a
Zen-CODE <zenkey.zencode@gmail.com>
2019-08-21 23:09:09 +0200

    Started with move to div

--- react/zentunez/src/components/PlayerState/PlayerState.js
+++ react/zentunez/src/components/PlayerState/PlayerState.js
@@ -6,10 +6,26 @@ export class PlayerState extends Component {
     /**
      * This component handles the display of the album cover, along with a
      * button to update it
-     */    
+     */
 
 
     render(){
+      //var img = ""; // "http://127.0.0.1:8000/library/cover/Professor%20Trance%20And%20The%20Energisers/Shaman'S%20Breath";
+      // var img = "http://127.0.0.1:8000/library/cover/Affiance/Blackout";
+      var img = encodeURI(this.props.img_src);
+      console.log("Player State: image=" + this.props.img_src);
+
+      var back_style = {
+        // backgroundImage: "url(http://127.0.0.1:8000/library/cover/Professor%20Trance%20And%20The%20Energisers/Shaman'S%20Breath)"
+        // backgroundImage: "url(" + this.props.img_src + ")"
+        backgroundImage: "url(" + img + ")",
+        height: '400px',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center'
+      };
+
+      // <MDBCol><div alt="Album cover" style={ back_style } onClick={ () => window.open(this.props.img_src) }></div></MDBCol>
       return (
         <MDBContainer>
           <MDBRow><MDBCol><b>Track: </b> { this.props.track }</MDBCol></MDBRow>
@@ -18,7 +34,7 @@ export class PlayerState extends Component {
             <MDBCol><input type="range" className="custom-range"  min="0" max="100" value={ this.props.position * 100 } readOnly={ true }/></MDBCol>
           </MDBRow>
           <MDBRow>
-            <MDBCol><img className="album-cover" alt="Album cover" src={ this.props.img_src } onClick={ () => window.open(this.props.img_src) }></img></MDBCol>
+            <MDBCol><div alt="Album cover" style={{ backgroundImage: "url(" + `${img}` + ")", height: "400px" }} onClick={ () => window.open(this.props.img_src) }></div></MDBCol>
           </MDBRow>
         </MDBContainer>
       );
