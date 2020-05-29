d1b3524581e9867f61acc46d104f798789388b4c
Zen-CODE <zenkey.zencode@gmail.com>
2020-05-29 21:54:55 +0200

    :sparkles: Changed adding to insert, updated deploy

--- react/zentunez/deploy.sh
+++ react/zentunez/deploy.sh
@@ -2,9 +2,10 @@
 echo "Running the NPM build...."
 npm run build
 echo "Copying static files to static..."
-rsync -avz "build/static" "../../python/django/tunez/"
+rsync -avz "build/static" "../../../zenplayer/zenplayer/webserver"
+rm -rf build/static
 echo "Copying react application files..."
-rsync -avz build/*.* "../../python/django/tunez/react/"
+rsync -avz build/*.* "../../../zenplayer/zenplayer/webserver/static"
 echo "Removing build..."
 rm -rf build/
 echo "All done!"
