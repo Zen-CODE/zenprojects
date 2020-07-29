c1890297b6a12bfc5a2863f934307697ebabdd6a
Zen-CODE <zenkey.zencode@gmail.com>
2020-07-29 22:29:53 +0200

    :rocket: Added external docs, grammar tweaks

--- python/fastapi/app/routers/library/library_api.py
+++ python/fastapi/app/routers/library/library_api.py
@@ -6,12 +6,12 @@ router = APIRouter()
 
 
 @router.get("/library/get_artists",
-            tags=["library"],
+            tags=["Library"],
             responses={404: {"description": "Library folder not found."}},
             response_model=ArtistListModel)
 async def get_artists():
     """
-    Return a list of artist in the music library.
+    Return a list of artists in the music library.
     """
     if False:
         raise HTTPException(status_code=404, detail="Library folder not found")
