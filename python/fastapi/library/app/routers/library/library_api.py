a55503f8d5499e53afff36781e2db4b78cf9bb46
fruitbat <zenkey.zencode@gmail.com>
2020-08-03 22:02:54 +0200

    :bulb: Fixed naming error

--- python/fastapi/library/app/routers/library/library_api.py
+++ python/fastapi/library/app/routers/library/library_api.py
@@ -48,7 +48,7 @@ async def get_cover(artist: str, album: str):
     """
     Return a list of albums for the specified artist.
     """
-    cover = library.get_cover_path(artist, album)
+    cover = library.get_cover(artist, album)
     if not cover:
         raise HTTPException(status_code=404, detail="Cover not found.")
     return FileResponse(cover, media_type="image/png")
