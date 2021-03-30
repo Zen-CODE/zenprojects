1050a45fed9cbc437c53b6ab61865fadd0a67240
richard <richard@dotmodus>
2021-03-30 14:20:29 +0200

    :contruction: Started with mechanics for FastAPI interaction

--- python/zenkeypy/src/main.py
+++ python/zenkeypy/src/main.py
@@ -1,16 +1,45 @@
 """This module houses the entrypoint for ZenKeyPy."""
 from config import Config
+from typing import Dict
+
+from fastapi import FastAPI
 
 from controller import Controller
 
 from hotkey_handler import HotKeyHandler
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    print("Launching ZenKeyPy...")  # noqa T001
-    config = Config.get_config()
-    ctrl = Controller(config["zenplayer_url"])
-    listener = HotKeyHandler.create_bindings(
-        config["hotkeymap"], ctrl)
-    ctrl.listener = listener
-    listener.join()
+
+class ZenKeyApp():
+    """This class houses the ZenKeyPy API interface."""
+
+    @staticmethod
+    def get_controller(wait: bool = False) -> Controller:
+        """Build and return a properly configured ZenKeyApp instance."""
+        config = Config.get_config()
+        ctrl = Controller(config["zenplayer_url"])
+        listener = HotKeyHandler.create_bindings(
+            config["hotkeymap"], ctrl)
+        ctrl.listener = listener
+        if wait:
+            print("ZenKeyApp - joining threads")
+            listener.join()
+
+        print("ZenKeyApp - returning controller.")
+        return ctrl
+
+    @staticmethod
+    @app.get("/")
+    def read_root() -> Dict:
+        """Return the root of out API."""
+        return {"Hello": "zen"}
+
+    @staticmethod
+    @app.get("history/")
+    def history() -> Dict:
+        """Return the list of actions in our history."""
+        return {"items": []}
+
+
+ctrl = ZenKeyApp.get_controller()
