742eeb537a80c1999fe1392a4e433d05f82dd681
Richard Larkin <zenkey.zencode@gmail.com>
2022-04-03 12:53:38 +0200

    :art: Added styling

--- python/django/zenplayer_ui/zenplayer/interaction.py
+++ python/django/zenplayer_ui/zenplayer/interaction.py
@@ -1,14 +1,16 @@
 """This file contains classes for interaction with the Zenplayer instance."""
 from dataclasses import dataclass, asdict
+from requests import get
+from json import loads
 
-ZENPLAYER_UWL = "http://127.0.0.1:9001/"
+ZENPLAYER_URL = "http://127.0.0.1:9001/"
 
 
 @dataclass
 class NowPlaying:
     artist: str
     album: str
-    track: int
+    track_number: int
 
 
 class ZenFetcher:
@@ -17,4 +19,10 @@ class ZenFetcher:
     @staticmethod
     def now_playing():
         """Return the 'Now playing' data."""
-        return asdict(NowPlaying("artist", "album", 1))
+        response = get(ZENPLAYER_URL + 'zenplaylist/get_current_info')
+        data = loads(response.content)
+        np = NowPlaying(
+            artist=data['artist'],
+            album=data['album'],
+            track_number=data['track_number'])
+        return asdict(np)
