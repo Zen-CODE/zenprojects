307158517398cf3a89b493e5bcfa2798b6b42c3d
Richard Larkin <zenkey.zencode@gmail.com>
2022-12-18 20:47:58 +0200

    :construction: Started with tests

---
+++ node/zentunez/tests/zenplaylist.test.js
@@ -0,0 +1,16 @@
+/*
+    This module houses the test for the ZenPlaylist functionality
+*/
+const supertest = require('supertest')
+const app = require('../app.js')
+const request = supertest(app)
+
+
+it('Test the we can fetch the "Now playing" data.', async () => {
+    const response = await request.get('/now_playing')
+  
+    expect(response.status).toBe(200)
+    // expect(response.body.message).toBe('pass!')
+
+  })
+  
