153d7f24647c1d64896b56497fac1cb5ab6f5ba2
Richard Larkin <zenkey.zencode@gmail.com>
2023-01-08 20:06:06 +0200

    :zap: Added platform, release, uptime

--- node/zentunez/app.js
+++ node/zentunez/app.js
@@ -24,13 +24,29 @@ app.get('/zen*', async (req, res) => {
 })
 
 
+con_to_time = (seconds) => {
+    const d = Math.floor(seconds / (3600*24))
+    const h = Math.floor(seconds % (3600*24) / 3600)
+    const m = Math.floor(seconds % 3600 / 60)
+    const s = Math.floor(seconds % 60)
+
+    const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : ""
+    const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : ""
+    const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : ""
+    const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : ""
+    return dDisplay + hDisplay + mDisplay + sDisplay
+}
+
 app.get('/sysinfo*', async (req, res) => {
     const json = {
         "Arch" : os.arch(),
         "CPU's": os.cpus(),
         "Free memory": (os.freemem() / (1024 ** 3)).toString() + " GB",
         "Total memory": (os.totalmem() / (1024 ** 3)).toString() + " GB",
-        "Load average (1, 5, 15min)": os.loadavg()
+        "Load average (1, 5, 15min)": os.loadavg(),
+        "Platform": os.platform(),
+        "Release": os.release(),
+        "Uptime": con_to_time(os.uptime())
     }
     res.json(json)
 })
