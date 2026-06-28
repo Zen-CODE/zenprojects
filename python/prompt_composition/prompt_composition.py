ce75a45a0f997663e1723d92a00e638ed1322efb
Richard Larkin <zenkey.zencode@gmail.com>
2026-06-28 08:26:39 +0200

    :fire: Cleanup

--- python/prompt_composition/prompt_composition.py
+++ python/prompt_composition/prompt_composition.py
@@ -4,6 +4,9 @@ from requests import get
 from functools import partial
 
 
+AUTH_DICT = dict[str, list[str]]
+"""A standard dict structure for holding user data. e.g {'roles': ['PP', 'PHA']}"""
+
 def get_data(url: str) -> dict:
     """Fetch arbitrary data"""
     print(" > Fetching data -> Pydantic model -|")
@@ -15,30 +18,34 @@ def get_members(policy_no: int) -> list:
     """Return a list of members for the policy"""
     return ["Bob Chop", "Harry Houndini", "The other Guy"]
 
+def has_all_auth(user_auth: AUTH_DICT, required_auth: AUTH_DICT) -> bool:
+    """Return True if the user has all on trhe required authentication, False otherwise"""                
+    for required_key, required_values in required_auth.items():
+        user_values = user_auth.get(required_key, [])
+        if not any(user_value in required_values for user_value in user_values):
+            return False
+    return True                                                        
+
 def process_prompt(raw_prompt_text: str, user_auth: dict[str, list[str]], fetches: dict[callable]) -> str:
     """Process the `raw_prompt_string` and insert data if role appropriate."""
 
     soup = BeautifulSoup("<root>" + raw_prompt_text + "</root>", "xml")
-    # for tag in soup.find_all("claim_data"):
+    fetch_keys = list(fetches.keys()) 
     for tag in soup.find_all(True):
-        fetch_name = tag.get("callable")
-        if fetch_name:
-            del tag["callable"]
-
-            # We have a tag for which data is defined
+        if tag.name in fetch_keys:
+            # We have a tag for which a data fetch is defined
             auth_attr = tag.get("auth")
-            if auth_attr:
+            authorised = not bool(auth_attr)
+            if not authorised:
                 required_auth: dict[str, list[str]] = ast.literal_eval(auth_attr)
-                authorised = False
-                for required_auth, required_values in required_auth.items():
-                    user_values = user_auth.get(required_auth, [])
-                    if any(user_value in required_values for user_value in user_values):
-                        authorised = True
-                        func = fetches[fetch_name]  # Get the function to call for the data based on the tag name
-                        tag.append(str(func()) + "\n")
+                authorised = has_all_auth(user_auth, required_auth)
                                                         
                 del tag["auth"]
-                if not authorised:
+                if authorised:
+                    # TODO: Collects the fetches and run async in parallel
+                    func = fetches[tag.name]  # Get the function to call for the data based on the tag name
+                    tag.append(str(func()) + "\n")  # TODO: model_dump_json
+                else:
                     tag.replace_with("Data omitted due to insufficient permissions.")
 
     final_output = "".join(str(child) for child in soup.find("root").contents)
@@ -49,25 +56,36 @@ markdown_template = """
 Prompt
 ======
 
-# Claim Analysis
-<sec_fence callable="claim_data" auth="{'roles': ['PP', 'PHA']}">
-</sec_fence>
+Claim Analysis
+--------------
+<get_claims  auth="{'roles': ['PP', 'PHA']}">
+</get_claims>
 
-# View Policy
-<sec_fence callable="list_members" auth="{'consents': ['VIEW_POLICY']}">
-</sec_fence>
+Admin and billing
+-----------------
+<get_admin_and_billing  auth="{'roles': ['PP', 'PHA']}">
+</get_admin_and_billing>
+
+Member list
+------------
+Authorisation requirements are processed as "AND" if in one auth item, "OR" if across multiple
+
+<get_member_list auth="{'consents': ['VIEW_POLICY']}" auth="{'roles': ['PP']}">
+User has view policy access or is a PP!
+</get_member_list>
 """
 
 
 fetches = {
-    "claim_data": partial(get_data, 'https://jsonplaceholder.typicode.com/todos/1'),
-    "list_members": partial(get_members, 1234)
+    "get_claims": partial(get_data, 'https://jsonplaceholder.typicode.com/todos/1'),
+    "get_admin_and_billing": lambda: {"admin": "admin stuff", "billing": "billing stuff"},
+    "get_member_list": partial(get_members, 1234)
 }
 """A dictionary defining functions to be called to insert data as defiend by the 'callable' attribute."""
 
 
 print("\n================================================  1. Spouse prompt")
-print(process_prompt(markdown_template, user_auth={"roles": ["CH"], "consents": []}, fetches=fetches))
+print(process_prompt(markdown_template, user_auth={"roles": ["SP"], "consents": []}, fetches=fetches))
 
 print("\n#================================================ 2. Principle pass")
 print(process_prompt(markdown_template, user_auth={"roles": ["PH", "PP"], "consents": ["VIEW_POLICY"]}, fetches=fetches))
